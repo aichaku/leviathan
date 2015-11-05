718d6f8f00e18c823a43a43fb4d35d02f30d5bf3
Dongwoo Lee <cognitive.lee@lge.com>
2015-11-05 15:05:58 +0900

    brute force method has been added and time measurement will take place

--- d/unique_string_identifier.cs
+++ d/unique_string_identifier.cs
@@ -4,9 +4,9 @@ namespace unique_string_identifier
 {
     class perse
     {
-        const int sizeOfAscii = 128;
+        static int sizeOfAscii = 128;
 
-        public bool isDuplicated(string a)
+        static public bool isDuplicated(string a)
         {
             if (a.Length > sizeOfAscii)
             {
@@ -30,16 +30,29 @@ namespace unique_string_identifier
         }
     }
 
+    class BruteForce
+    {
+        static public bool isDuplicated(string a)
+        {
+
+            for (var x = 0; x < a.Length; x++)
+                for (var i = x + 1; i < a.Length; i++)
+                    if (a[i] == a[x]) return true;
+            return false;
+        }
+    }
+
     class Program
     {
+
         static void Main(string[] args)
         {
             string s1 = "abcdefghijklmnopqrstuvwxyz";
             string s2 = "abcdefghijklmnapqrstuvwxyz";
 
-            perse p = new perse();
+            DateTime timer = DateTime.Now;
 
-            if (p.isDuplicated(s1))
+            if (perse.isDuplicated(s1))
             {
                 Console.WriteLine("it has duplicated characters.");
             } else
@@ -47,7 +60,7 @@ namespace unique_string_identifier
                 Console.WriteLine("They are all unique characters.");
             }
 
-            if (p.isDuplicated(s2))
+            if (perse.isDuplicated(s2))
             {
                 Console.WriteLine("it has duplicated characters.");
             }
@@ -55,6 +68,32 @@ namespace unique_string_identifier
             {
                 Console.WriteLine("They are all unique characters.");
             }
+
+            TimeSpan timeGap = DateTime.Now - timer;
+            Console.WriteLine("{0}:{1}", timeGap, timeGap.Milliseconds);
+
+            timer = DateTime.Now;
+
+            if (BruteForce.isDuplicated(s1))
+            {
+                Console.WriteLine("it has duplicated characters.");
+            }
+            else
+            {
+                Console.WriteLine("They are all unique characters.");
+            }
+
+            if (BruteForce.isDuplicated(s2))
+            {
+                Console.WriteLine("it has duplicated characters.");
+            }
+            else
+            {
+                Console.WriteLine("They are all unique characters.");
+            }
+
+            timeGap = DateTime.Now - timer;
+            Console.WriteLine("{0}:{1}", timeGap, timeGap.Milliseconds);
         }
     }
 }
