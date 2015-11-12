682d7028f794491ef9e47b83f209d4a5b26e1faf
Dongwoo Lee <cognitive.lee@lge.com>
2015-11-12 10:37:51 +0900

    answer to Q3 and this resolves #6

---
+++ d/week1/round_3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack3 s = new Stack3(30, 4);
+
+            s.push(0, 1);
+            s.push(2, 1);
+            s.push(3, 1);
+            s.push(1, 1);
+            s.push(2, 2);
+            s.push(2, 3);
+            s.push(2, 4);
+            s.push(2, 4);
+            s.push(2, 4);
+            s.push(2, 4);
+            s.push(2, 4);
+            s.push(2, 4);
+            s.push(3, 2);
+            s.push(3, 3);
+            s.push(3, 4);
+            s.push(3, 5);
+            s.push(3, 6);
+            s.push(3, 7);
+            s.push(3, 8);
+            s.push(3, 9);
+            s.push(3, 10);
+            s.push(3, 11);
+
+            foreach (int i in s.getArray())
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(s.pop(3));
+            }
+
+
+        }
+    }
+}
---
+++ d/week1/round_3/Stack3.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1
+{
+    class Stack3
+    {
+        private int[] s;
+        private int numOfStack;
+        private int[] top;
+        private int[] bottom;
+
+        public Stack3(int size, int n)
+        {
+            s = new int[size];
+            numOfStack = n;
+            top = new int[n];
+            bottom = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                top[i] = -1;
+                bottom[i] = i * size / n;
+            }
+        }
+
+        public int[] getTop()
+        {
+            return top;
+        }
+
+        public int[] getArray()
+        {
+            return s;
+        }
+
+        private bool isOverFlow(int i)
+        {
+            int position = top[i];
+
+            if (++position + bottom[i] == s.Length) return true;
+            if (i < numOfStack - 1 && position + bottom[i] == bottom[i + 1]) return true;
+            
+            return false;
+        }
+
+        public void push(int id, int value)
+        {
+            if (isOverFlow(id))
+            {
+                Console.WriteLine("unable to add [" + value + "] due to stack overflow detected.");
+                return;
+            }
+
+            top[id]++;
+            s[top[id] + bottom[id]] = value;
+
+        }
+
+        public int pop(int id)
+        {
+            if (top[id] == -1)
+            {
+                Console.WriteLine("unable to pop due to empty stack");
+                return ~0;
+            }
+
+            return s[top[id]-- + bottom[id]];
+        }
+
+
+    }
+}
