d26ef25ba3e7e30baf707640598fe708babb81c2
Dongwoo Lee <cognitive.lee@lge.com>
2015-11-19 11:37:46 +0900

    interim upload for #8

---
+++ d/week1/round_5/w1q5_bitwise_manipulation/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w1q5_bitwise_manipulation
+{
+
+    class Bit32
+    {
+        private Int32 i;
+
+        public Int32 v
+        {
+            get { return i; }
+            set { i = value; }
+        }
+
+        public String toString()
+        {
+
+            /*
+            string s = i.ToString();
+            string s = Convert.ToString(i);
+            string s = string.Format("{0}", i);
+            string s = "" + i;
+            string s = string.Empty + i;
+            string s = new StringBuilder().Append(i).ToString();
+            */
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int m = 0; m < 32; m++)
+            {
+                if (true) //(i & (1 << m))
+                {
+                    builder.Append(1);
+                } else
+                {
+                    builder.Append(0);
+                }
+            }
+
+
+            return i.ToString();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
